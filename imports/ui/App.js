e115431315b9fdb15bb6ab9aa8ff81ac5ffcc2e2
AlyneAlvarenga <alynevs@gmail.com>
2020-05-16 12:09:06 -0400

    linked form to database

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Items } from '../api/items.js';
 // import { Hello } from './Hello.jsx';
 // import { Info } from './Info.jsx';
 
 const App = (props) => {
-  
-  // getItems = () => {
-  //   return [
-  //     { _id: 1, text: 'Pineapple on Pizza' },
-  //     { _id: 2, text: 'No Pineapple on Pizza' },
-  //   ]
-  // }
+  const [question, setQuestion] = useState('');
+  const [item1, setItem1] = useState('');
+  const [item2, setItem2] = useState('');
+
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    Items.insert({
+      question,
+      first: { item1: item1, count: 0 },
+      second: { item2: item2, count: 0 },
+      createdAt: new Date(),
+    })
+
+    setQuestion('');
+    setItem1('');
+    setItem2('');
+  }
 
   renderItems = () => {
     return props.items.map(item => {
       return (
-        <li key={item._id}>{item.text}</li>
+        <li key={item._id}>
+          <h2>{item.question}</h2>
+          <button>{item.first.item1}</button>
+          <button>{item.second.item2}</button>
+        </li>
       )
     })
   }
@@ -25,15 +41,19 @@ const App = (props) => {
         <header>
           <h1>Versus</h1>
 
-          <form action="">
+          <form action="" onSubmit={handleSubmit}>
             <label htmlFor="question">Add your question</label>
-            <input type="text" id="question" placeholder="Question" />
+            <input type="text" id="question" placeholder="Question" name="question" value={question}
+            onChange={e => setQuestion(e.target.value)}/>
 
             <label htmlFor="item1">Item 1</label>
-            <input type="text" id="item1" placeholder="Item 1" />
+            <input type="text" id="item1" placeholder="Item 1" name="item1" value={item1}
+            onChange={e => setItem1(e.target.value)}/>
 
             <label htmlFor="item2">Item 2</label>
-            <input type="text" id="item2" placeholder="Item 2" />
+            <input type="text" id="item2" placeholder="Item 2" name="item1" value={item2}
+            onChange={e => setItem2(e.target.value)}/>
+            <button>Submit</button>
           </form>
         </header>
 
